3e7042360f7a23c1904121a4cc5f5641163c4a4f
Zhao Shulin <zhaoshulin.cn@gmail.com>
2014-07-07 11:29:51 +0800

    second commit

--- CarreerCup_Cracking_the_Technical_Interview/1_6.c
+++ CarreerCup_Cracking_the_Technical_Interview/1_6.c
@@ -41,28 +41,65 @@ int get_value(int matrix[], int i, int j)//此函数的功能是返回matrix[i][
 	exit(EXIT_FAILURE); // should never goto this line!
 }
 
-
-int rotate_90(char * matrix)
+int print_matrix(int *matrix)
 {
-	int len, N, t, tmp, i, j, m, n;
-	
+	int len, N, i, j;
 	for(len = 0; matrix[len] != '\0'; len ++);
 	len --;
 	
 	for(N = 0; N*N < len; N ++);
 
-	for(t = 0; t < len/2; t ++)
+	for(i = 0; i < N; i ++)
+	{
+		for(j = 0; j < N; j++)
+		{
+			printf("%d ", get_value(matrix, i, j));
+		}
+		printf("\n");
+	}	
+	
+	return 0;
+}
+
+int rotate_90(int * matrix)
+{
+	int len, N, t, i, j, m, n;
+	
+	for(len = 0; matrix[len] != '\0'; len ++);
+	len --;
+	
+	for(N = 0; N*N < len; N ++);
+	printf("len is %d, N is %d.\n", len, N);
+
+	//求转置开始
+	for(t = 0; t < len; t ++)
 	{
 		i = t / N;
 		j = t % N;
-		if(i != j)
+		if(i < j)
 		{
 			m = get_value(matrix, i, j);
 			n = get_value(matrix, j, i);
 			matrix[j*N+i] = m;
 			matrix[i*N+j] = n;
 		}
-	}//求转置	
+	}//求转置结束
+
+	//逆排列开始
+	for(j = 0; j < N/2; j ++)
+	{
+		for(i = 0; i < N; i ++)
+		{
+			m = get_value(matrix, i, j);
+			n = get_value(matrix, i, N-1-j);
+			matrix[i*N+N-1-j] = m;
+			matrix[i*N+j] = n;
+		}
+	
+	}//逆排列结束
+	
+
+		
 	return 0;
 } 
 
@@ -70,6 +107,17 @@ int main(void)
 {
 	printf("Testing matrix...\n");
 	int matrix[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	get_value(matrix, 1, 1);
+	print_matrix(matrix);
+	printf("\n");
+	rotate_90(matrix);
+	print_matrix(matrix);
+	
+//	printf("\n");
+//	int matrix_2[4] = {1, 2, 3, 4};
+//	print_matrix(matrix_2);
+//	printf("\n");
+//	rotate_90(matrix_2);
+//	print_matrix(matrix_2);
+	
 	return 0;
 }
