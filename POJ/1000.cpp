aa351cc90517b9b3ac8d7110d5fc72ca4834c81d
Zhao Shulin <zhaoshulin.cn@gmail.com>
2014-09-23 15:52:06 +0800

    OpenJudge using c++

---
+++ POJ/1000.cpp
@@ -0,0 +1,18 @@
+/**
+ * Description: Calculate a + b
+ * Date: 2014/09/23
+ * Author: Zhaoshulin.cn@gmail.com
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << a+b << endl;
+	
+	return 0;
+}
