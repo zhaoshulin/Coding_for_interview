73858a49a99e5e8028b7daddd8e3f55106ec98a4
Zhao Shulin <zhaoshulin.cn@gmail.com>
2015-01-12 10:10:04 +0800

    1000
    ZOJ 1000.c

---
+++ ZOJ/1000.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+int main(void)
+{
+ int a, b;
+ while(scanf("%d %d", &a, &b) != EOF){
+   printf("%d\n", a+b);
+ }
+ 
+  return 0;
+}
+/**************************************************************
+    Problem: 1000
+    User: GENERAL__ZHAO
+    Language: C
+    Result: Accepted
+    Time:0 ms
+    Memory:912 kb
+****************************************************************/
